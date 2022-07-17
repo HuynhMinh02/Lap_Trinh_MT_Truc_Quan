b870779de4023a080167e823223862d2fb7e3708
HuynhMinh02 <huynhuuminh908@gmail.com>
2022-07-17 18:26:38 +0700

    update homework

---
+++ DocSoThanhChu/DocSoThanhChu/Form1.cs
@@ -0,0 +1,117 @@
+﻿namespace DocSoThanhChu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bntThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bntXoa_Click(object sender, EventArgs e)
+        {
+            txtRead.ResetText();
+            txtWrite.ResetText();
+        }
+        private String donVi(int n)
+        {
+            switch (n)
+            {
+                case 0: return "";
+                case 1: return "Một";
+                case 2: return "Hai";
+                case 3: return "Ba";
+                case 4: return "Bốn";
+                case 5: return "Năm";
+                case 6: return "Sáu";
+                case 7: return "Bảy";
+                case 8: return "Tám";
+                default: return "Chín";
+            }
+        }
+        private String hangChuc(int n)
+        {
+            switch (n)
+            {
+                case 0: return "Linh";
+                case 1: return "Mười";
+                case 2: return "Hai Mươi";
+                case 3: return "Ba Mươi";
+                case 4: return "Bốn Mươi";
+                case 5: return "Năm Mươi";
+                case 6: return "Sáu Mươi";
+                case 7: return "Bảy Mươi";
+                case 8: return "Tám Mươi";
+                default: return "Chín Mươi";
+            }
+        }
+        private String hangTram(int n)
+        {
+            switch (n)
+            {
+                case 0: return "Không Trăm";
+                case 1: return "Một Trăm";
+                case 2: return "Hai Trăm";
+                case 3: return "Ba Trăm";
+                case 4: return "Bốn Trăm";
+                case 5: return "Năm Trăm";
+                case 6: return "Sáu Trăm";
+                case 7: return "Bảy Trăm";
+                case 8: return "Tám Trăm";
+                default: return "Chín Trăm";
+            }
+        }
+
+        private void bntGiai_Click(object sender, EventArgs e)
+        {
+            string s, s1, s2, s3, k1, k2, k3;
+            int d, c, t;
+            s = txtWrite.Text;
+
+            if (s.Length == 1)
+            {
+                k1 = s.Substring(s.Length - 1, 1);
+                d = int.Parse(k1);
+                s1 = donVi(d);
+                txtRead.Text = s1;
+            }
+            else if (s.Length == 2)
+            {
+                k1 = s.Substring(s.Length - 1, 1);
+                k2 = s.Substring(s.Length - 2, 1);
+                d = int.Parse(k1);
+
+                c = int.Parse(k2);
+                s1 = donVi(d);
+                s2 = hangChuc(c);
+                txtRead.Text = s2 + " " + s1 + " ";
+            }
+            else if (s.Length == 3)
+            {
+                k1 = s.Substring(s.Length - 1, 1);
+                k2 = s.Substring(s.Length - 2, 1);
+                k3 = s.Substring(s.Length - 3, 1);
+                d = int.Parse(k1);
+                c = int.Parse(k2);
+                t = int.Parse(k3);
+                s1 = donVi(d);
+                s2 = hangChuc(c);
+                s3 = hangTram(t);
+                txtRead.Text = s3 + " " + s2 + " " + s1 + " ";
+            }
+            else
+            {
+                txtRead.Text = "Số quá lớn để đọc!!!";
+            }
+        }
+    }
+}
---
+++ TimSoLonNhat(2 số)/TimSoLonNhat/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace TimSoLonNhat
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txta = new System.Windows.Forms.TextBox();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.bntGiai = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txta
+            // 
+            this.txta.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txta.Location = new System.Drawing.Point(156, 102);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(149, 29);
+            this.txta.TabIndex = 0;
+            // 
+            // txtb
+            // 
+            this.txtb.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtb.Location = new System.Drawing.Point(156, 163);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(149, 29);
+            this.txtb.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(109, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "TÌM SỐ LỚN NHẤT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(12, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "NHẬP SỐ A:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(12, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "NHẬP SỐ B:";
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtKetQua.Location = new System.Drawing.Point(25, 264);
+            this.txtKetQua.Multiline = true;
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.Size = new System.Drawing.Size(351, 81);
+            this.txtKetQua.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(11, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 22);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "KẾT QUẢ:";
+            // 
+            // bntGiai
+            // 
+            this.bntGiai.BackColor = System.Drawing.Color.Lime;
+            this.bntGiai.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bntGiai.ForeColor = System.Drawing.Color.Red;
+            this.bntGiai.Location = new System.Drawing.Point(65, 374);
+            this.bntGiai.Name = "bntGiai";
+            this.bntGiai.Size = new System.Drawing.Size(84, 44);
+            this.bntGiai.TabIndex = 7;
+            this.bntGiai.Text = "GIẢI";
+            this.bntGiai.UseVisualStyleBackColor = false;
+            this.bntGiai.Click += new System.EventHandler(this.bntGiai_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.Color.Lime;
+            this.btnThoat.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnThoat.ForeColor = System.Drawing.Color.Red;
+            this.btnThoat.Location = new System.Drawing.Point(256, 374);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(91, 44);
+            this.btnThoat.TabIndex = 8;
+            this.btnThoat.Text = "THOÁT";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(403, 450);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.bntGiai);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtKetQua);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.txta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txta;
+        private TextBox txtb;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtKetQua;
+        private Label label4;
+        private Button bntGiai;
+        private Button btnThoat;
+    }
+}
